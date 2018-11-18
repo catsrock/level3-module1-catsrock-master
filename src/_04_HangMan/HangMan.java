038f9e6c2b4b28b18011128d9f2e51e1e67a4fa0
catsrock <catsrock58@gmail.com>
2018-11-18 14:33:18 -0800

    Started Hangman
    Play Hangman.jar next class :)
    Also, saving through GitHub desktop is iffy :(

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,13 @@
 package _04_HangMan;
 
+import javax.swing.JOptionPane;
+
 public class HangMan{
-	
+	public static void main(String[] args) {
+		String answer=JOptionPane.showInputDialog("Give a number from 1-266 inclusive");
+		int num1=Integer.parseInt(answer);
+		Utilities.getTotalWordsInFile("dictionary.txt");
+	}
 	
 	
 }
