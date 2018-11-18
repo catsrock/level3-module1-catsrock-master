ab6893a15af20d3d1556c12f8eed2ab9c6eccb72
catsrock <catsrock58@gmail.com>
2018-11-18 14:31:54 -0800

    Updated
    Finished this recipe

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -9,12 +9,14 @@ import org.junit.Test;
 
 public class _03_TestMatchingBrackets {
 	Stack <String> thing=new Stack<String>();
+	char c;
 	public static void main(String[] args) {
-		
+		new _03_TestMatchingBrackets();
 	}
-		
+	public _03_TestMatchingBrackets() {
+		doBracketsMatch("");
+	}
 	@Test
-
 	public void testMatchingBrackets() {
 		assertTrue(doBracketsMatch("{}"));
 		assertTrue(doBracketsMatch("{{}}"));
@@ -25,10 +27,31 @@ public class _03_TestMatchingBrackets {
 
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-	if () {
 		
-	}
-		return false;
-	}
-
+		for (int i = 0; i <b.length() ; i++) {
+			c=b.charAt(i);
+			if (c=='{') {
+			thing.push(b);
+			}
+			
+			if (c=='}') {
+			if (thing.isEmpty()) {
+				return false;
+			}
+			else {
+			thing.pop();
+			}
+			
+		}
+		
+		}
+		if (thing.isEmpty()) {
+			return true;
+		}
+		else {
+			return false;
+		
+			
+		}
+		}
 }
